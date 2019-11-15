ea68ceafd862eb662f36aad90c2ca29867ab05e4
yt8492 <tmymyut2000@gmail.com>
2019-11-15 22:52:13 +0900

    get access token

--- src/main/kotlin/api/json/TokenResponse.kt
+++ src/main/kotlin/api/json/TokenResponse.kt
@@ -1,9 +1,12 @@
-package json
+package api.json
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class TokenResponse(
+    @SerialName("token_type")
     val tokenType: String,
+    @SerialName("access_token")
     val accessToken: String
 )
